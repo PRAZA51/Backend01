e8b322d9a626e37ea089ed344f1e2ef14e05ffd8
PRAZA51 <pritamhazari17@gmail.com>
2026-01-17 20:08:26 +0530

    video_lecture_comp

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controler.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, updateAccountDetails, getUserChannelProfile } from "../controllers/user.controler.js";
 import {upload} from "../middlewears/multer.middlewears.js"
+import { verify } from "jsonwebtoken";
 // import { JsonWebTokenError } from "jsonwebtoken";
 
 const router = Router()
@@ -24,6 +25,13 @@ router.route("/register").post(
     //secure routes
     router.route("/logout").post(verifyJWT, logoutUser)
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+    router.route("/current-user").get(verifyJWT, getCurrentUser)
+    router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+    router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+    router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+    router.route("/c/:username").get(varifyJWT, getUserChannelProfile)
+    router.route("/history").get(varifyJWT, getWatchHistory)
 
 
 
